6764c66e3ceb36d2f554e48d7f12adf013927908
megane9988 <info@m-g-n.me>
2025-06-20 19:16:38 +0900

    Initial Commit

---
+++ LLMTextTranslator/LLMTextTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMTextTranslatorApp.swift
+//  LLMTextTranslator
+//
+//  Created by 9988 megane on 2025/06/20.
+//
+
+import SwiftUI
+
+@main
+struct LLMTextTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
